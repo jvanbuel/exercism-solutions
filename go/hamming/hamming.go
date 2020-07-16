81a50e3374e5ca5b87361226aedb439ef4637744
je76893 <jan.vanbuel@kbc.be>
2020-07-16 12:40:35 +0200

    add hamming and raindrops

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -5,15 +5,20 @@ import "fmt"
 
 // Distance calculates the Hamming distance between two strings a and b
 func Distance(a, b string) (int, error) {
-	if len(a) != len(b) {
-		return 0, fmt.Errorf("String a has length %d and string b has length %d", len(a), len(b))
+
+	var aRune = []rune(a)
+	var bRune = []rune(b)
+
+	if len(aRune) != len(bRune) {
+		return 0, fmt.Errorf("string a has length %d and string b has length %d", len(a), len(b))
 	}
 
-	var h int = 0
-	for i, c := range a {
-		if b[i] != byte(c) {
+	var h int
+	for i, c := range aRune {
+		if bRune[i] != c {
 			h++
 		}
 	}
+
 	return h, nil
 }
---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,8 @@
+// Package raindrops contains the solution to the raindrops exercise of the Exercism go track
+package raindrops
+
+// Convert converts the number of raindrops n into
+func Convert(n int) string {
+
+	return "Hello,World!"
+}
