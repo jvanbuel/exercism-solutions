c0d2b943ddef10a1933c8c98cdf17364b1490b9c
Jan <janvanbuel@hotmail.com>
2020-07-16 18:35:26 +0200

    Implemented scrabble_score solution

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,1 +1,35 @@
+// Package scrabble contains the solution to the scrabble problem of the Exercism go track
 package scrabble
+
+import "strings"
+
+// Score calculates the scrabble score of a string s
+func Score(s string) int {
+	var score int
+
+	for _, c := range strings.ToUpper(s) {
+		score += scoreChar(string(c))
+	}
+	return score
+}
+
+func scoreChar(c string) int {
+
+	switch c {
+	case "A", "E", "I", "O", "U", "L", "N", "R", "S", "T":
+		return 1
+	case "D", "G":
+		return 2
+	case "B", "C", "M", "P":
+		return 3
+	case "F", "H", "V", "W", "Y":
+		return 4
+	case "K":
+		return 5
+	case "J", "X":
+		return 8
+	case "Q", "Z":
+		return 10
+	}
+	return 0
+}
