43f083501124ada884f0bb53a205f8c1a273de81
je76893 <jan.vanbuel@kbc.be>
2020-07-16 13:02:49 +0200

    Solution to raindrops problem

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,8 +1,23 @@
 // Package raindrops contains the solution to the raindrops exercise of the Exercism go track
 package raindrops
 
-// Convert converts the number of raindrops n into
-func Convert(n int) string {
+import "strconv"
 
-	return "Hello,World!"
+// Convert converts the number of raindrops n into a string according to some specific rules
+func Convert(n int) string {
+	var s string
+	if n%3 == 0 {
+		s += "Pling"
+	}
+	if n%5 == 0 {
+		s += "Plang"
+	}
+	if n%7 == 0 {
+		s += "Plong"
+	}
+	if s == "" {
+		return strconv.Itoa(n)
+	}
+
+	return s
 }
