3ecb5266d360cb5273119e55945c8a31793a7bfe
je76893 <jan.vanbuel@kbc.be>
2020-08-03 14:22:45 +0200

    solution to bob problem

---
+++ go/bob/bob.go
@@ -0,0 +1,32 @@
+// Package bob should have a package comment that summarizes what it's about.
+package bob
+
+import "strings"
+
+// Hey should have a comment documenting it.
+func Hey(remark string) string {
+	r := strings.Replace(remark, " ", "", -1)
+	r = strings.Replace(r, "\t", "", -1)
+	r = strings.Replace(r, "\n", "", -1)
+	r = strings.Replace(r, "\r", "", -1)
+	switch r {
+	case "":
+		return "Fine. Be that way!"
+	case strings.ToLower(r):
+		if r[len(r)-1] == '?' {
+			return "Sure."
+		}
+		return "Whatever."
+	case strings.ToUpper(r):
+		if r[len(r)-1] == '?' {
+			return "Calm down, I know what I'm doing!"
+		}
+		return "Whoa, chill out!"
+	default:
+		if r[len(r)-1] == '?' {
+			return "Sure."
+		}
+		return "Whatever."
+	}
+
+}
