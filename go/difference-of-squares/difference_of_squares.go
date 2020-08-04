11af3af22b7242bb21ec8313da9e237032c370c9
je76893 <jan.vanbuel@kbc.be>
2020-08-04 11:27:25 +0200

    solution to difference_of_squares

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,17 @@
+// Package diffsquares is a solution to the difference of squares problem of the Exercism go track
+package diffsquares
+
+// SquareOfSum calculates the square of the sum of the first i integers
+func SquareOfSum(i int) int {
+	return i * i * (i + 1) * (i + 1) / 4
+}
+
+// SumOfSquares calculates the sum of the squares of the first i integers
+func SumOfSquares(i int) int {
+	return i * (i + 1) * (2*i + 1) / 6
+}
+
+// Difference calculates the difference between the square of sum and the sum of squares
+func Difference(i int) int {
+	return SquareOfSum(i) - SumOfSquares(i)
+}
