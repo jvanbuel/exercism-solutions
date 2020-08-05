d2cefc105c8428bdec17e58194bc220dba8e403f
Jan <janvanbuel@hotmail.com>
2020-08-05 21:39:31 +0200

    updated solution to differences of squares problem

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -3,7 +3,8 @@ package diffsquares
 
 // SquareOfSum calculates the square of the sum of the first i integers
 func SquareOfSum(i int) int {
-	return i * i * (i + 1) * (i + 1) / 4
+	sum := i * (i + 1) / 2
+	return sum * sum
 }
 
 // SumOfSquares calculates the sum of the squares of the first i integers
