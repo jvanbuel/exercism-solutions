ead44e4e83ef422ade11ee35772490fd44e966e3
Jan <janvanbuel@hotmail.com>
2020-08-06 20:37:59 +0200

    solution to nth prime

---
+++ go/nth-prime/nth_prime.go
@@ -0,0 +1,34 @@
+package prime
+
+// Nth returns the nth prime
+func Nth(n int) (int, bool) {
+	if n == 0 {
+		return 0, false
+	}
+	var i int
+	count := 0
+
+	for count != n {
+		i++
+		if IsPrime(i) {
+			count++
+		}
+	}
+	return i, true
+}
+
+// IsPrime determines whether and integer is prime or not
+func IsPrime(i int) bool {
+	if i == 1 {
+		return false
+	}
+	if i == 2 {
+		return true
+	}
+	for j := 2; j < i; j++ {
+		if i%j == 0 {
+			return false
+		}
+	}
+	return true
+}
