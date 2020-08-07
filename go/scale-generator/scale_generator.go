33b072bf2855deb6d6d2cbd3712714074a9eba31
Jan <janvanbuel@hotmail.com>
2020-08-07 13:56:18 +0200

    solution to scale generator problem

--- go/scale-generator/scale_generator.go
+++ go/scale-generator/scale_generator.go
@@ -1,24 +1,48 @@
 package scale
 
+import (
+	"strings"
+)
+
 // Scale returns a scale based on a tonic t  and an interval string i
-func Scale(t string, is string) []string {
-	var d []string
-	switch t {
-	case "G", "D", "A", "E", "B", "F#", "e", "b", "f#", "c#", "g#", "d#":
-		d = []string{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"}
+func Scale(tonic string, steps string) []string {
+	var diatomic []string
+	switch tonic {
+	case "C", "G", "D", "A", "E", "B", "F#", "a", "e", "b", "f#", "c#", "g#", "d#":
+		diatomic = []string{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"}
 	case "F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb":
-		d = []string{"F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "E"}
+		diatomic = []string{"F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E"}
 	}
-	if is == "" {
-		return d
+	if steps == "" {
+		steps = strings.Repeat(" ", 12)
 	}
 
-	m := map[rune]int{'m': 1, 'M': 2}
+	var currentI int
 	var scale []string
-	startI := 
 
-	for _, i := range is {
-		step := m[i]
+	if len(tonic) == 2 {
+		tonic = strings.ToUpper(string(tonic[0])) + string(tonic[1])
+	} else {
+		tonic = strings.ToUpper(string(tonic[0]))
+	}
+
+	for i, v := range diatomic {
+		if tonic == v {
+			currentI = i
+			scale = append(scale, v)
+		}
+	}
+
+	for _, step := range steps[:len(steps)-1] {
+		switch step {
+		case 'M':
+			currentI += 2
+		case 'A':
+			currentI += 3
+		default:
+			currentI++
+		}
+		scale = append(scale, diatomic[(currentI)%12])
 
 	}
 
