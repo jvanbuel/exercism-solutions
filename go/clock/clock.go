f5b262173a8d98b33b0c1a4584c70a1f65227133
Jan <janvanbuel@hotmail.com>
2020-08-14 20:41:10 +0200

    updated solution to clock problem

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -4,42 +4,31 @@ import "fmt"
 
 // Clock is structure consisting of an hours an minutes field
 type Clock struct {
-	hour   int
 	minute int
 }
 
 // New returns a new clock
 func New(hour, minute int) Clock {
+	minutes := hour*60 + minute
+	if minutes < 0 {
+		return Clock{minutes%(60*24) + 60*24}
+	}
 	return Clock{
-		hour:   posRemainder(hour+carry(minute), 24),
-		minute: posRemainder(minute, 60),
+		minute: minutes % (60 * 24),
 	}
 }
 
 // String returns a string representation of a clock
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+	return fmt.Sprintf("%02d:%02d", c.minute/60, c.minute%60)
 }
 
 // Add adds minutes to a Clock
 func (c Clock) Add(minutes int) Clock {
-	return New(c.hour, c.minute+minutes)
+	return New(0, c.minute+minutes)
 }
 
 // Subtract substracts minutes from a Clock
 func (c Clock) Subtract(minutes int) Clock {
-	return New(c.hour, c.minute-minutes)
-}
-
-// Carry calculates the number of hours that need to be carried to the hour field of a clock
-func carry(minutes int) int {
-	if minutes < 0 {
-		return (minutes+1)/60 - 1
-	}
-	return minutes / 60
-}
-
-// posRemainder calculates the positive remainder of the division of i by m
-func posRemainder(i int, m int) int {
-	return (i%m + m) % m
+	return New(0, c.minute-minutes)
 }
