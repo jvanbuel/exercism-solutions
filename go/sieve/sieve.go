6795ad0a34ce927793ffac276b0478c740a334ae
Jan <janvanbuel@hotmail.com>
2020-08-15 13:19:20 +0200

    solution to sieve problem

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,1 +1,31 @@
 package sieve
+
+// Sieve finds all primes smaller or equal to n
+func Sieve(n int) []int {
+	if n <= 0 {
+		return []int{}
+	}
+	r := make([]int, n-1)
+	for i := range r {
+		r[i] = i + 2
+	}
+
+	for i := 0; i < len(r); i++ {
+		if r[i] == 0 {
+			continue
+		}
+		j := i + r[i]
+		for j < len(r) {
+			r[j] = 0
+			j += r[i]
+		}
+	}
+
+	var primes []int
+	for _, v := range r {
+		if v != 0 {
+			primes = append(primes, v)
+		}
+	}
+	return primes
+}
