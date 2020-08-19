b45975d13f88676609ee88dc7bfb45a263cf542f
Jan <janvanbuel@hotmail.com>
2020-08-19 22:00:34 +0200

    download allergies problem

---
+++ go/allergies/allergies.go
@@ -0,0 +1,11 @@
+package allergies
+
+// Allergies returns a list of allergicResults
+func Allergies(i uint) []string {
+	return []string{}
+}
+
+// AllergicTo determines wheter someone is allergic to a substance s
+func AllergicTo(i uint, s string) bool {
+	return false
+}
