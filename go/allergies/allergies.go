890918d0b59b653bbbc6decdfea026a2e1b589be
Jan <janvanbuel@hotmail.com>
2020-08-19 22:14:48 +0200

    solution to allergies problem

--- go/allergies/allergies.go
+++ go/allergies/allergies.go
@@ -1,11 +1,33 @@
 package allergies
 
+var allergens = []string{
+	"eggs",
+	"peanuts",
+	"shellfish",
+	"strawberries",
+	"tomatoes",
+	"chocolate",
+	"pollen",
+	"cats",
+}
+
 // Allergies returns a list of allergicResults
-func Allergies(i uint) []string {
-	return []string{}
+func Allergies(n uint) []string {
+	var allergies []string
+	for i, v := range allergens {
+		if (n >> i % 2) == 1 {
+			allergies = append(allergies, v)
+		}
+	}
+	return allergies
 }
 
 // AllergicTo determines wheter someone is allergic to a substance s
-func AllergicTo(i uint, s string) bool {
+func AllergicTo(n uint, s string) bool {
+	for i, v := range allergens {
+		if s == v {
+			return (n >> i % 2) == 1
+		}
+	}
 	return false
 }
