f7c1d683baf12c0a16574fbe9bd1d481f658c01f
Jan <janvanbuel@hotmail.com>
2021-09-16 10:43:30 +0200

    low-power-embedded-game exercise

---
+++ rust/low-power-embedded-game/src/lib.rs
@@ -0,0 +1,18 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    return (dividend / divisor, dividend % divisor);
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.enumerate().filter(|x| x.0 % 2 == 0).map(|x| x.1)
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        self.0.abs() + self.1.abs()
+    }
+}
